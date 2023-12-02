517acf14f396feea8730437cc8a7d7627dd4bdf2
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-12-03 02:01:47 +0530

    Updated

---
+++ 19Closure.js
@@ -0,0 +1,15 @@
+// Closure will remember lexical environment variables
+let a="global";
+
+function outerPrint(){
+  let b="outer";
+  return function innerPrint(){
+    let c="inner";
+    return `${a}->${b}->${c}`;
+  }
+}
+
+const show=outerPrint();
+console.log(show);
+const printInner=show();
+console.log(printInner);
