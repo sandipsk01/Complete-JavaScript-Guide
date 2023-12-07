a34d123a49d495965f5053762a4ff6af0f65d552
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-12-07 15:08:49 +0530

    Update and rename 3strings.html to 3strings.js

---
+++ 3strings.js
@@ -0,0 +1,13 @@
+
+        var name='Sandip';
+        var about='I am working in TCS';
+        //String Constructor
+        var stringconstructor= new String("String Constructor");
+        console.log(stringconstructor);
+        // Use ${} to get value of variables
+        var myInfo=`My name is ${name} and ${about}`;
+        console.log(myInfo);
+        //Escape Sequence like \n , \t , etc,,.
+        console.log("fsfsfssf sdjsfsjd fsfsdfs fsfgegg edgdgd dgbdg \ngfsfdd \t\tsdsdf");
+
+    
