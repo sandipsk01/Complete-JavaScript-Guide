766193c1f189e223e32feb3946dbdf436b90d208
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-12-29 12:32:43 +0530

    Object.create(), Object.freeze(), Object.assign()

--- 28Objectcreate.js
+++ 28Objectcreate.js
@@ -9,3 +9,13 @@ const car={
 const car1=Object.create(car);
 console.log(car1);   //{} prototype Object with getDetails method
 car1.getDetails("Audi");
+
+
+// Object.freeze() static method freezes an object
+// Object.freeze(obj)
+
+// The Object.assign() static method copies all enumerable own properties from one or more source objects to a target object. It returns the modified target object.
+// Object.assign(target)
+// Object.assign(target, source1)
+// Object.assign(target, source1, source2)
+// Object.assign(target, source1, source2, /* …, */ sourceN)
