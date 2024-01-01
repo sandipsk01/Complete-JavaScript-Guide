d4ab0897a2ad953e66f6214b36f6b08ae042ac70
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2024-01-01 21:11:56 +0530

    Create 31ES6Classes.js

---
+++ 31ES6Classes.js
@@ -0,0 +1,11 @@
+// Classes are just a syntactical sugar for constructor function.
+// Class can be hoisted
+ // class ClassName{
+      // Properties (optional)
+      // Constructor (only when values needs to be initiated)
+      // Methods
+// }
+
+// Class declaration
+class Vehicle{
+  name;
