8b5e2a6838b95c7e8fa27228051f8ab2038b3f46
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2024-01-03 21:12:24 +0530

    Create 37ShallowAndDeepCopy.js

---
+++ 37ShallowAndDeepCopy.js
@@ -0,0 +1,25 @@
+const obj1={
+  name:"John",
+  age:12,
+  marks:{
+    maths:34,
+    english:80
+  }
+}
+
+//Shallow copy: Copying first layer values
+const obj2={...obj1};
+// const obj2=Object.assign({},obj1);
+// const obj2=obj1.slice();
+obj2.name="Doe";   // Will not effect obj1: First layer can not change bcoz of shallow copy
+obj2.marks.maths=70;  // Will effect obj1: shallow copy doesn't work on Inner layer 
+console.log(obj1);
+console.log(obj2);
+
+//Deep Copy
+const obj3=JSON.parse(JSON.stringify(obj1));
+obj3.name="Sandy";
+obj3.marks.maths=90;
+console.log(obj1);
+console.log(obj3);
+//Note: JSON will not work on Methods
